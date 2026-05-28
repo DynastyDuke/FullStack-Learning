f199ec08968d58e1ec5cde866900062722eaf911
DynastyDuke <dynasty03072@gmail.com>
2026-05-29 01:23:41 +0800

    renew

---
+++ src/Practice/NameCard.java
@@ -0,0 +1,23 @@
+package Practice;
+
+public class NameCard {
+
+	public static void main(String[] args) {
+
+		String name = "Levi";
+		int ages = 29;
+		float height = 172.5f;
+		float weight = 69.7f;
+		boolean isStudent = false;
+
+		System.out.println("===================");
+		System.out.println("姓名：" + name);
+		System.out.println("年齡：" + ages + "歲");
+		System.out.println("身高：" + height + "公分");
+		System.out.println("體重：" + weight + "公斤");
+		System.out.println("在學中：" + isStudent);
+		System.out.println("===================");
+
+	}
+
+}
